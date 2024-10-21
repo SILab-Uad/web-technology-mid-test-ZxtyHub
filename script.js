71df916f6ca22d037725131acd2e480bb50195ef
ZxtyHub <zxty07@gmail.com>
2024-10-21 20:04:33 +0700

    Attempt 1 - 14.15

--- script.js
+++ script.js
@@ -8,8 +8,25 @@ const generatePassword = (length, options) => {
     const specialChars = "!@#$%^&*()";
 
     // TODO: Create a variable for the character set based on selected options
+    let setCharacter = "";
+    
+    if (options.includeUppercase) setCharacter += uppercase;
+    if (options.includeLowercase) setCharacter += lowercase;
+    if (options.includeNumbers) setCharacter += numbers;
+    if (options.includeSpecialChars) setCharacter += specialChars;
+
+    // Make sure at least one character type is selected
+    if (setCharacter === "") {
+        return "Please select at least one character type!";
+    }
 
     // TODO: Generate the password based on the selected criteria
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * setCharacter.length);
+        password += setCharacter[randomIndex];
+    }
+
     return password;
 };
 
